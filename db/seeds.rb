f4de76a9d6648f11ec9b85aed9b01eb757208586
Kevin Trinh <kxt.trinh@gmail.com>
2022-11-08 19:01:27 +0100

    vue seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,22 +7,62 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 require "faker"
+require "open-uri"
+
 
 User.destroy_all
 Product.destroy_all
+Category.destroy_all
 
 User.create!(email: "kev@mail.com",
              password: '123456',
              password_confirmation: "123456",
              admin: true )
 
-25.times do |i|
-  Product.create!(name: Faker::Food.dish,
-                  # description: Faker::ChuckNorris.fact,
-                  description: Faker::Food.description,
-                  price: rand(1..20),
-                  catering: true )
-end
+cat1 = Category.create!(heading: 'Pizza', body: "It's so good dough!", display: true)
+cat2 = Category.create!(heading: 'Pasta', body: "We cannoli do so much in each video", display: true)
+cat3 = Category.create!(heading: 'Desserts', body: "Rockj rails icecream", display: true)
 
+10.times do |i|
+  pizza = Product.new(name: 'Pizza',
+                      # description: Faker::ChuckNorris.fact,
+                      description: Faker::Food.description,
+                      price: 11.99,
+                      catering: true,
+                      category: Category.find(cat1.id)
+                      )
+
+  pasta = Product.new(name: 'Pasta',
+                      # description: Faker::ChuckNorris.fact,
+                      description: Faker::Food.description,
+                      price: 7.95,
+                      catering: true,
+                      category: Category.find(cat2.id)
+                      )
+  dessert = Product.new(name: 'Dessert',
+                      # description: Faker::ChuckNorris.fact,
+                      description: Faker::Food.description,
+                      price: 3.00,
+                      catering: true,
+                      category: Category.find(cat3.id)
+                      )
+
+  # file = URI.open('https://picsum.photos/1920/1080')
+
+  pizza.image.attach(io: URI.open('https://picsum.photos/1920/1080'), filename: "#{i}_pizza_image.jpg")
+  pasta.image.attach(io: URI.open('https://picsum.photos/1920/1080'), filename: "#{i}_pasta_image.jpg")
+  dessert.image.attach(io: URI.open('https://picsum.photos/1920/1080'), filename: "#{i}_dessert_image.jpg")
+
+  if i == 5
+    pizza.featured = true
+    pasta.featured = true
+    dessert.featured = true
+  end
+
+  pizza.save
+  pasta.save
+  dessert.save
+
+end
 
 puts "finished !!"
